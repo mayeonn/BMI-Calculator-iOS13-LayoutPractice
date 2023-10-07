6ce554313f6aa56f65049a3f711b7b90e7cc51dd
mayeonn <mayeonn9@gmail.com>
2023-10-07 17:18:41 +0900

    Change weight and height value according to each UISlider's value

--- BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
+++ BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var height: UILabel!
+    @IBOutlet weak var weight: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func heightChanged(_ sender: UISlider) {
+        height.text = String(format: "%.2fm", sender.value)
+    }
+    @IBAction func weightChanged(_ sender: UISlider) {
+        weight.text = String(format: "%dkg", Int(sender.value))
+    }
+    
 }
 
